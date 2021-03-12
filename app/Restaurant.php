968753e3163068913b717a37672dcdf0d8047c55
Simone <simone.bervi@gmail.com>
2021-03-12 16:46:17 +0100

    Creati fillable restaurant

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -5,17 +5,28 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
-{
-    public function user() {
-      return $this->belongsTo('App\User');
-    }
+{   
+  protected $fillable = [
+    'user_id',
+    'address',
+    'phone_number',
+    'website',
+    'email_rest',
+    'image',
+    'vat',
+    'slug'
+  ];
+  
+  public function user() {
+    return $this->belongsTo('App\User');
+  }
 
-    public function types() {
-      return $this->belongsToMany('App\RestType');
-    }
+  public function types() {
+    return $this->belongsToMany('App\RestType');
+  }
 
-    public function foods() {
-      return $this->hasMany('App\Food');
-    }
+  public function foods() {
+    return $this->hasMany('App\Food');
+  }
 
 }
