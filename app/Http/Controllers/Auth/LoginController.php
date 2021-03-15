393d787e5c1b453cd1cb29a4eb0eba6df66fb2ec
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-15 15:34:47 +0100

    Login->viewIndex, Logout->viewWelcome

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,6 +43,6 @@ class LoginController extends Controller
 
     public function logout(Request $request) {
       Auth::logout();
-      return redirect()->route('home');
+      return redirect()->route('welcome');
     }
 }
