517d9675993107d6f237c0302989b47b5a7fdcc9
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-15 10:54:06 +0100

    Fix vista Show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -6,9 +6,8 @@
         <h2 class="display-4 text-center">{{ $dish->name}}</h2>
         {{-- <h3 class="text-center mb-4">{{ $dish->ingredients}}</h3> --}}
         <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name}}" style="width: 500px">
-        </div>
-        <p class="mt-4">{{ $dish->ingredients }}</p>
-        <p><strong>Ingredienti: </strong>{{ $dish->description }}</p>
+        <p class="mt-4 mb-2"><strong>Ingredienti: </strong>{{ $dish->ingredients }}</p>
+        <p>{{ $dish->description }}</p>
         {{-- <p class="mb-4"><strong>Data di pubblicazione: </strong>{{ $dishes->publication_date }}</p> --}}
 
         <a href="{{ route('admin.dishes.index') }}" class="btn btn-lg btn-outline-primary">Torna alla home</a>
