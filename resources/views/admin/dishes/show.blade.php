9b34aa0e85ed7d770c329293d5b09c6d8455305d
francesco <f.iacono89@gmail.com>
2021-03-15 13:25:10 +0100

    view-admin-show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -3,14 +3,47 @@
 @section('content')
     
     <div class="container my-4">
-        <h2 class="display-4 text-center">{{ $dish->name}}</h2>
-        {{-- <h3 class="text-center mb-4">{{ $dish->ingredients}}</h3> --}}
-        <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name}}" style="width: 500px">
-        <p class="mt-4 mb-2"><strong>Ingredienti: </strong>{{ $dish->ingredients }}</p>
-        <p>{{ $dish->description }}</p>
-        {{-- <p class="mb-4"><strong>Data di pubblicazione: </strong>{{ $dishes->publication_date }}</p> --}}
-
-        <a href="{{ route('admin.dishes.index') }}" class="btn btn-lg btn-outline-primary">Torna alla home</a>
+        <h2 class="display-4">{{ $dish->name}}</h2>
+        @if ($dish->vegetarian =! 0) 
+          <span class="badge badge-success mb-2" style="font-size: 120%;"> Vegetarian </span>     
+        @endif
+        <div>
+            <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name}}" style="width: 100%;  border: 10px solid #F0F0F1">
+        </div>
+        <table class="table table-light table-striped table-bordered">
+            <tr>                
+                <td style="width: 150px">
+                    <strong>Ingredienti: </strong>
+                </td>
+                <td>
+                    {{ $dish->ingredients }}
+                </td>
+            <tr>
+                <td style="width: 150px">
+                    <strong>Descrizione: </strong> 
+                </td>
+                <td>
+                    {{ $dish->description }}
+                </td>     
+            </tr>
+            <tr>
+                <td style="width: 150px">
+                    <strong>Prezzo: </strong> 
+                </td>
+                <td>
+                    {{ number_format($dish->price, 2) }} &euro; 
+                </td>     
+            </tr>
+            <tr>
+                <td style="width: 150px">
+                    <strong>Creato il: </strong> 
+                </td>
+                <td>
+                    {{ date('j F, Y', strtotime($dish->created_at)) }} 
+                </td>     
+            </tr>        
+        </table>
+        <a href="{{ route('admin.dishes.index') }}" class="btn btn-lg btn-dark">Torna alla home</a>
     </div>
 
 @endsection
