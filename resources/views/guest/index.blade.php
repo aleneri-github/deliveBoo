663b458c11d89624d3fd966622b50a4b86eb3007
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-16 12:11:46 +0100

    Index Guest con la nostra Palette Colori

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 
-  <main class="container back-color">
+  <main class="container">
 
     <h1>pagina index di guest - prima pagina del sito</h1>
 
-    <div id="jumbotron" class="bg-info p-4">
+    <div id="jumbotron" class="p-4">
       <h3>Jumbotron - slider con i ristoranti piu votati</h3>
     </div>
 
-    <div id="types" class="bg-warning p-4">
+    <div id="types" class="p-4">
       <h3>Types - Navbar con le tipologie di ristorante</h3>
     </div>
 
-    <div id="restaurants" class="bg-success p-4">
+    <div id="restaurants" class="p-4">
       <h3>Restaurants - Elenco dei ristoranti</h3>
     </div>
 
