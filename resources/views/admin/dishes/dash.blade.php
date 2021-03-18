0c71006715bee7fff9f72118d6f295800f6d3522
francesco <f.iacono89@gmail.com>
2021-03-18 23:48:31 +0100

    Dashboard responsive

--- resources/views/admin/dishes/dash.blade.php
+++ resources/views/admin/dishes/dash.blade.php
@@ -14,20 +14,29 @@
       <nav class="admin-menu">
         <ul>
           <li>
-            <i class="fas fa-cubes"></i>
-            <a href="#">Dashboard</a> 
+            
+            <a href="#">
+              <i class="fas fa-cubes"></i>  
+              <span>Dashboard</span>
+            </a> 
           </li>
           <li>
-            <i class="fas fa-hamburger"></i>
-            <a href="#">Menu</a>
+            <a href="#">
+              <i class="fas fa-hamburger"></i>
+              <span>Menu</span>
+            </a>
           </li>
           <li>
+            <a href="">
             <i class="fas fa-chart-line"></i>
-            <a href="">Statistiche</a>
+              <span>Statistiche</span>
+            </a>
           </li>
           <li>
-            <i class="fas fa-utensils"></i>
-            <a href="">Ordini</a>
+            <a href="">
+              <i class="fas fa-utensils"></i>
+              <span>Ordini</span>
+            </a>
           </li>
         </ul>
   
@@ -37,56 +46,74 @@
   </div>
   {{-- main --}}
   <main id="admin-show">
-    <h1>Ciao Peppe</h1>
+    <h1>Ciao Peppe!</h1>
 
   </main>
   {{-- pannello logout --}}
   <div id="admin-info">
     <div class="top">
       <!-- Right Side Of Navbar -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Authentication Links -->
-      @guest
-          <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-          </li>
-          @if (Route::has('register'))
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-              </li>
-          @endif
-      @else
-          <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }}
-              </a>
+      <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
 
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                      onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
-                  </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
 
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                      @csrf
-                  </form>
-              </div>
-          </li>
-      @endguest
-    </ul>
-  </div>
-
-  <div class="info-side">
-    <div class="card-restaurant">
-      <img src="https://www.mymi.it/wp-content/uploads/2017/07/Ceresio-7-terrazza-ok-min.jpg" alt="">
-      <h4>Ceresio 7</h4>
-      <p>info@ceresio.com</p>
-      <span class="badge badge-dark">Italiano</span>
-      <span class="badge badge-dark">Vegetariano</span>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+      </ul>
     </div>
-  </div>
-    
+    {{-- info restaurant --}}
+    <div class="info-side">
+      <div class="card-restaurant">
+        <img src="https://www.mymi.it/wp-content/uploads/2017/07/Ceresio-7-terrazza-ok-min.jpg" alt="">
+        <h4>Ceresio 7</h4>
+        <p>info@ceresio.com</p>
+        <span class="badge badge-dark">Italiano</span>
+        <span class="badge badge-dark">Vegetariano</span>
+      </div>
+    </div>
+    {{-- dati restaurant --}}
+    <div class="data">
+      <div class="data-top">
+          <div class="box">
+            <span>2400</span>
+            <p>Ordini</p>
+          </div>
+          <div class="box">
+            <span>20</span>
+            <p>Food</p>
+          </div>
+          <div class="box">
+            <span>12400</span>
+            <p>Incasso</p>
+          </div>
+      </div>
+
+    </div>
+
 </div>
 </section> 
 
