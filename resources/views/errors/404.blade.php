2b0724fea9cddba48a7a98d4a54646676827d990
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-24 18:45:21 +0100

    Pagina 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.front')
-
+@extends('layouts.guest')
 @section('content')
 <div id="page-404" >
     <div class="container">
-        <h1>Ops nemmeno Adriano è riusito a trovare la tua pagina</h1>
-        {{-- <img src="css/img/tenor.gif" alt="" srcset=""> --}}
+        <h1>Ops!!</h1>
+        <h3>Ci dispiace ma nemmeno Adamo ha trovato il tuo piatto</h3>
+        <img src="{{ asset('img/404.jpg') }}" alt="Creazione di Adamo">
     </div>
 </div>
 @endsection
