d59c46dcda6646b503ec11cada56197707c0b027
francesco <f.iacono89@gmail.com>
2021-03-24 17:44:33 +0100

    fix 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts.front')
+@extends('layouts.guest')
 
 @section('content')
 <div id="page-404" >
     <div class="container">
-        <h1>Ops nemmeno Adriano è riusito a trovare la tua pagina</h1>
-        {{-- <img src="css/img/tenor.gif" alt="" srcset=""> --}}
+        <h1>Ops!!</h1>
+        <h3>Ci dispiace ma nemmeno Adamo ha trovato il tuo piatto</h3>
+        <img src="{{ asset('img/404.png') }}" alt="" srcset="">
     </div>
 </div>
 @endsection
