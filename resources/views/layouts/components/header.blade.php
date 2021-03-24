e8fa2e75d344f2b231120d964386d0680f11f7d5
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-24 11:31:45 +0100

    Pusho per cambiare branch

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -1,57 +1,55 @@
 <div id="app">
-  <nav class="navbar navbar-expand-md shadow-sm">
-      <div class="container">
-          {{-- <a class="navbar-brand" href="{{ url('/') }}">
-              {{ config('<img src="../img/deliverYou-Logo.svg" alt="">', '<img src="../img/deliverYou-Logo.svg" alt="">') }}
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-              <span class="navbar-toggler-icon"></span>
-          </button> --}}
+    <nav class="navbar navbar-expand-md shadow-sm">
+        <div class="container">
+            {{-- <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('<img src="../img/deliverYou-Logo.svg" alt="">', '<img src="../img/deliverYou-Logo.svg" alt="">') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button> --}}
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <!-- Left Side Of Navbar -->
-              <ul class="navbar-nav mr-auto">
-                
-                {{-- LOGO A SINISTRA --}}
-                <a href="{{ url('/') }}">
-                    <img src="{{ asset('img/deliverYou-Logo.svg') }}" alt="deliverYou-logo">
-                    <span><strong>DeliverYou</strong></span>
-                </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- PARTE SINISTRA -->
+                <ul class="navbar-nav mr-auto">
+                    {{-- LOGO --}}
+                    <a href="{{ url('/') }}">
+                        <img src="{{ asset('img/deliverYou-Logo.svg') }}" alt="deliverYou-logo">
+                        <span><strong>DeliverYou</strong></span>
+                    </a>
+                </ul>
 
-              </ul>
+                <!-- PARTE DESTRA -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Link di Autenticazione -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
 
-              <!-- Right Side Of Navbar -->
-              <ul class="navbar-nav ml-auto">
-                  <!-- Authentication Links -->
-                  @guest
-                      <li class="nav-item">
-                          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                      </li>
-                      @if (Route::has('register'))
-                          <li class="nav-item">
-                              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                          </li>
-                      @endif
-                  @else
-                      <li class="nav-item dropdown">
-                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                              {{ Auth::user()->name }}
-                          </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
 
-                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                              <a class="dropdown-item" href="{{ route('logout') }}"
-                                 onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
-                                  {{ __('Logout') }}
-                              </a>
-
-                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                  @csrf
-                              </form>
-                          </div>
-                      </li>
-                  @endguest
-              </ul>
-          </div>
-      </div>
-  </nav>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
