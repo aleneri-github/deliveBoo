9529641a5c5c1bf8c8e5e1e0123cf6213b1c4a18
Alessandro <aleneri242@gmail.com>
2021-03-26 22:21:18 +0100

    mail

--- resources/views/mail/checkout.blade.php
+++ resources/views/mail/checkout.blade.php
@@ -4,9 +4,15 @@
     <meta charset="utf-8">
     <title></title>
   </head>
-  <body>
-    <h3>Complimenti! Ordine effettuato con successo!!</h3>
-    <img src="{{ asset('img/deliverYou.svg') }}" alt="">
-
+  <body style="box-sizing:border-box; margin:0; padding:0; height:100vh;">
+    <header style="box-sizing:border-box; margin:0; height:20vh; padding-left: 40px; background-color:#FDC234;">
+      <img style="height:100%;" src="{{ asset('img/deliverYou-bianco.svg') }}" alt="logo">
+    </header>
+    <main style="box-sizing:border-box; margin:0; padding:15px; height:60vh; display:flex; flex-direction:column; justify-content:center;">
+      <h3 style="font-family:sans-serif; text-align:center; color:#7a7a7a;">Complimenti! Ordine effettuato con successo!!</h3>
+      <img style="height:130px;" src="{{ asset('img/food-rest.svg') }}" alt="">
+      <h3 style="font-family:sans-serif; text-align:center; color:#7a7a7a;">Grazie per averci scelto!</h3>
+    </main>
+    <footer style="box-sizing:border-box; margin:0; height:20vh; background-color:#FDC234;"></footer>
   </body>
 </html>
