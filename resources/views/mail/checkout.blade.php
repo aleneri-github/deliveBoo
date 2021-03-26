7fc50bffc5834fefe8cff34228ec827a9bebefb5
Alessandro <aleneri242@gmail.com>
2021-03-26 18:28:16 +0100

    mail-checkout

--- resources/views/mail/checkout.blade.php
+++ resources/views/mail/checkout.blade.php
@@ -4,9 +4,19 @@
     <meta charset="utf-8">
     <title></title>
   </head>
-  <body>
-    <h1>Ordine effettuato con successo</h1>
-    
+  <body style="height:100vh; box-sizing:border-box; margin:0; padding:0;">
+    <header style="height:15vh; background-color:#FDC234; padding:5px; box-sizing:border-box;">
+      <img style="height:100%;" src="{{ asset('img/white-logo.png')}}" alt="logo-deliveBoo">
+
+    </header>
+    <main style="height:70vh; display:flex; flex-direction: column; align-items:center; box-sizing:border-box; margin:0; padding:25px;">
+      <h2 style="color:#7a7a7a; text-align:center; box-sizing:border-box; margin:0; padding:0; font-family:sans-serif;">Ordine effettuato con successo</h2>
+      <img style="height:160px; "src="{{ asset('img/food-rest.svg') }}" alt="image-hamburger">
+      <h2 style="color:#7a7a7a; text-align:center; box-sizing:border-box; margin:0;padding:0; font-family:sans-serif;">Grazie per averci scelto!!</h2>
+    </main>
+    <footer style="height:15vh; background-color: #FDC234;box-sizing:border-box; margin:0;padding:0;"></footer>
+
+
 
   </body>
 </html>
