971d3503df124c5322fdbe6e7a92350d969f22d6
francesco <f.iacono89@gmail.com>
2021-03-27 15:53:13 +0100

    fix Header

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -1,25 +1,21 @@
-<header id="app">
-    <nav class="navbar navbar-expand-md shadow-sm">
+<header class="header-app">
+    <nav class="navbar navbar-expand-md navbar-light shadow-sm">
         <div class="container">
-            {{-- <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('img/white-logo.png') }}" alt="logo-deliveBoo">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
-            </button> --}}
+            </button>
 
-            <div class="navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-                <!-- PARTE SINISTRA -->
-                {{-- <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                </ul> --}}
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
-                    <img src="{{ asset('img/white-logo.png') }}" alt="logo-deliveBoo">
-                </a>
+                </ul>
 
-                <!-- PARTE DESTRA -->
-                <ul class="navbar-nav">
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
@@ -31,9 +27,6 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Su di noi</a>
-                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
@@ -41,8 +34,9 @@
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">Esci
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    Esci
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
