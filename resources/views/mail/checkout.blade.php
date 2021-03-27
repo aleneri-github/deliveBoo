90284f351c8431ab78d7c64a16ec6aff0d9d3322
Alessandro <aleneri242@gmail.com>
2021-03-27 22:58:06 +0100

    layout mails

--- resources/views/mail/checkout.blade.php
+++ resources/views/mail/checkout.blade.php
@@ -2,21 +2,28 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
     <title></title>
   </head>
-  <body style="box-sizing:border-box; margin:0; padding:0; height:100vh;">
-    <header style="box-sizing:border-box; margin:0; height:20vh; padding-left: 40px; background-color:#FDC234;">
+  <body style="box-sizing:border-box; margin:0; padding:0; border: 7px solid #00B08C;">
+    <header style="box-sizing:border-box; margin:0; height:20vh; padding-left: 40px; background-color:#FDC234; border-bottom: 2px solid white; ">
       <img style="height:100%;" src="{{ asset('img/deliverYou-bianco.svg') }}" alt="logo">
     </header>
-    <main style="box-sizing:border-box; margin:0; padding:15px; height:60vh; display:flex; flex-direction:column; justify-content:center;">
-      <h3 style="font-family:sans-serif; text-align:center; color:#7a7a7a;">
-        Complimenti {{ $order->name. ' ' .$order->surname }}! Ordine effettuato con successo!!
-      </h3>
-      <p>Indirizzo: {{ $order->buyer_address}}</p>
-      <h4><strong>Totale: {{ $order->total }}</strong></h4>
-      <img style="height:130px;" src="{{ asset('img/food-rest.svg') }}" alt="">
-      <h3 style="font-family:sans-serif; text-align:center; color:#7a7a7a;">Grazie per averci scelto!</h3>
+    <main style="background-image: url('https://previews.123rf.com/images/nightman1965/nightman19651205/nightman1965120500003/13434243-muro-di-mattoni-texture-di-alta-qualit%C3%A0.jpg'); background-size: cover; background-repeat: no-repeat; box-sizing:border-box; margin:0; padding:15px; display:flex; justify-content:center;">
+      <div style="background-color: rgba(255, 255, 255, 0.7); width:70%; display:flex; flex-direction:column; justify-content:center; align-items: center; border-radius:20px; border: 4px solid #00B08C;)">
+        <h3 style="font-family: 'Barlow Condensed', sans-serif; font-size:28px; text-align:center; color:#00B08C; ">
+          Complimenti <strong>{{ $order->name. ' ' .$order->surname }}</strong>! <br> Ordine effettuato con successo!!
+        </h3>
+        <img style="height:130px;" src="{{ asset('img/food-rest.svg') }}" alt="">
+        <h3 style="font-family: 'Barlow Condensed', sans-serif; font-size:28px; text-align:center; color:#00B08C; ">Grazie per averci scelto!</h3>
+        <div style="box-sizing:border-box; border: 4px solid #00B08C; border-radius: 20px; padding:5px; width:90%; margin:15px;">
+          <h4 style="font-family: 'Barlow Condensed', sans-serif; text-align:center; font-size:22px; color:#00B08C;  margin:10px;">Indirizzo di consegna: <br> <strong>{{ $order->buyer_address}}</strong></h4>
+
+            <h4 style="font-family: 'Barlow Condensed', sans-serif; text-align:center; font-size:22px; color:#00B08C;  margin:0px;">Totale: <strong>{{ $order->total }} &euro;</strong></h4>
+        </div>
+      </div>
     </main>
-    <footer style="box-sizing:border-box; margin:0; height:20vh; background-color:#FDC234;"></footer>
+    <footer style="box-sizing:border-box; margin:0; height:20vh; background-color:#FDC234; border-top: 2px solid white;"></footer>
   </body>
 </html>
--- resources/views/mail/deliveyou.blade.php
+++ resources/views/mail/deliveyou.blade.php
@@ -2,12 +2,21 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
     <title></title>
   </head>
-  <body>
-    <h3>Benvenuto {{ $user->name }}!!</h3>
-    <h3>Registrazione a DeliverYou effettuata con successo, entra nell'area amministratore per gestire il tuo ristorante e aggiungere nuovi piatti!</h3>
-    <img src="{{ asset('img/deliverYou.svg') }}" alt="">
+  <body style="box-sizing:border-box; margin:0; padding:0; border: 7px solid #00B08C;">
+    <header style="box-sizing:border-box; margin:0; height:20vh; padding-left: 40px;    background-color:#FDC234; border-bottom: 2px solid white; ">
+      <img style="height:100%;" src="{{ asset('img/deliverYou-bianco.svg') }}" alt="logo">
+    </header>
+    <main style="background-image: url('https://previews.123rf.com/images/nightman1965/nightman19651205/nightman1965120500003/13434243-muro-di-mattoni-texture-di-alta-qualit%C3%A0.jpg'); background-size: cover; background-repeat: no-repeat; box-sizing:border-box; margin:0; padding:15px; display:flex; justify-content:center;">
+    <div style="box-sizing:border-box; border: 4px solid #00B08C; border-radius: 20px; padding:5px; width:90%; margin:15px; background-color: rgba(255, 255, 255, 0.7);">
+      <h3 style="font-family: 'Barlow Condensed', sans-serif; font-size:28px; text-align:center; color:#00B08C;">Benvenuto {{ $user->name }}!</h3>
+      <p style="font-family: 'Barlow Condensed', sans-serif; font-size:22px; text-align:center; color:#00B08C;">La tua registrazione a DeliveBoo è andata a buon fine, accedi all'area amministratore per aggiungere nuovi piatti.</p>
+    </div>
 
+    </main>
+    <footer style="box-sizing:border-box; margin:0; height:20vh; background-color:#FDC234; border-top: 2px solid white;"></footer>
   </body>
 </html>
